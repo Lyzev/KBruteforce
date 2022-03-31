b7679e3271f216feedd29ec0bb3a49f20cb54242
Lyzev <lyzev.info@gmail.com>
2022-03-31 23:15:50 +0200

    dokka test
    Signed-off-by: Lyzev <lyzev.info@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
+import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.net.URL
 
 plugins {
     kotlin("jvm") version "1.5.10"
@@ -16,8 +18,18 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:${project["dokka_version"]}")
+tasks.withType<DokkaTask>().configureEach {
+    dokkaSourceSets {
+        named("main") {
+            moduleName.set("Dokka Gradle Documentation")
+            includes.from("README.md")
+            sourceLink {
+                localDirectory.set(file("src/main/kotlin"))
+                remoteUrl.set(URL("https://github.com/Lyzev/KBruteforce/tree/gh-pages/dokka"))
+                remoteLineSuffix.set("#L")
+            }
+        }
+    }
 }
 
 tasks.withType<KotlinCompile>() {
