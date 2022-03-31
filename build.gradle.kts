f73f52c10f484a9895ee4b0706ff225d2b64ce73
Lyzev <lyzev.info@gmail.com>
2022-03-31 20:33:02 +0200

    added plugin for dokka docs
    Signed-off-by: Lyzev <lyzev.info@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,20 @@ plugins {
     kotlin("jvm") version "1.5.10"
 }
 
+operator fun Project.get(property: String): String {
+    return property(property) as String
+}
+
 group = "me.lyzev"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
-
+dependencies {
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:${project["dokka-gradle-plugin_version"]}")
+}
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "13"
